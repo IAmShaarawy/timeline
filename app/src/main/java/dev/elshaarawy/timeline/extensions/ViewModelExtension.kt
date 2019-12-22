bdf30e1712056e6e8818c9ddd755dbb430636687
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2019-12-22 19:44:37 +0200

    feat: add two extension to the ViewModel to use cotoutines more easily

---
+++ app/src/main/java/dev/elshaarawy/timeline/extensions/ViewModelExtension.kt
@@ -0,0 +1,22 @@
+package dev.elshaarawy.timeline.extensions
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+fun ViewModel.launch(
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+) = viewModelScope.launch(context, start, block)
+
+fun ViewModel.async(
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+) = viewModelScope.async(context, start, block)
