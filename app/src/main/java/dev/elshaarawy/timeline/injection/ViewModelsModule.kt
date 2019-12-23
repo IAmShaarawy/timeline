609aadeb33af5d6da4da1b67b012e4217dd3352a
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2019-12-24 00:00:39 +0200

    feat: setup navigation

--- app/src/main/java/dev/elshaarawy/timeline/injection/ViewModelsModule.kt
+++ app/src/main/java/dev/elshaarawy/timeline/injection/ViewModelsModule.kt
@@ -1,9 +1,9 @@
 package dev.elshaarawy.timeline.injection
 
 import dev.elshaarawy.timeline.app.AppViewModel
-import dev.elshaarawy.timeline.app.TimeLineApp
 import dev.elshaarawy.timeline.features.login.LoginViewModel
 import dev.elshaarawy.timeline.features.settings.SettingsViewModel
+import dev.elshaarawy.timeline.features.splash.SplashViewModel
 import dev.elshaarawy.timeline.features.timeline.TimelineViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
@@ -12,6 +12,7 @@ import org.koin.dsl.module
 object ViewModelsModule : () -> Module {
     override fun invoke(): Module = module {
         viewModel { AppViewModel(get()) }
+        viewModel { SplashViewModel() }
         viewModel { LoginViewModel() }
         viewModel { TimelineViewModel() }
         viewModel { SettingsViewModel() }
